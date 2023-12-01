6db5632a893d1e5b8c74588933be3eb84853b8a3
DAWID <G00388437@atu.ie>
2023-12-01 10:50:42 +0000

    adding

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -39,9 +39,16 @@ const bookSchema = new mongoose.Schema({
 
 const bookModel = mongoose.model('my_books', bookSchema);
 
+app.delete('/api/book:id',async (req,res,)=>
+{
+  console.log("delete: "+req.params.id);
+  let book = await bookModel.findByIdAndDelete(req.params.id);
+  res.send(book);
+})
+
 app.put('/api/book/:id', async(req,res)=>{
   console.log("Update: "+req.params.id);
-  let book = await bookModel.findByIdAndUpdate(req.params.id, req.body,{new:true});
+  let book = await bookModel.findByIdAndUpdate(req,params.id, req.body,{new:true});
   res.send(book);
 })
 
